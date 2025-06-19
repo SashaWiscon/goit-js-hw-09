71e31be8f5be8964012c097b3ee5371dc42f963c
SashaWiscon <108764697+SashaWiscon@users.noreply.github.com>
2025-06-19 22:16:13 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,28 +5,28 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      if (shouldResolve) { 
+        resolve({ position, delay });
         } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`); 
+         reject({ position, delay });
       }
     }, delay);
   });
 }
 
+
+
 function onPromiseSubmit(evt) {
   evt.preventDefault();
-  const delay = evt.currentTarget.delay;
-  const step = evt.currentTarget.step;
-  const amount = evt.currentTarget.amount;
+  const delay = Number(evt.currentTarget.delay.value);
+  const step = Number(evt.currentTarget.step.value);
+  const amount = Number(evt.currentTarget.amount.value);
 
-  let timerId;
-  let amountInter = Number(amount.value);
-  let positionP = 1;
-  let delayP = Number(delay.value);
+  let delayP = delay;
+  //  let delayP = Number(delay.value);
 
-  timerId = setInterval(() => {
-    createPromise(positionP, delayP)
+  for (let i = 1; i <= amount; i+=1) {
+    createPromise(i, delayP)
       .then(value => {
         console.log(
           `✅ Fulfilled promise ${value.position} in ${value.delay}ms`
@@ -38,14 +38,47 @@ function onPromiseSubmit(evt) {
         );
       });
 
-    positionP += 1;
-    amountInter -= 1;
-    delayP += Number(step.value);
-
-    if (!amountInter) {
-      clearInterval(timerId);
-    }
-  }, Number(step.value));
+    delayP += step;
+  }
 }
+
+
+
+// function onPromiseSubmit(evt) {
+//   evt.preventDefault();
+//   const delay = evt.currentTarget.delay;
+//   const step = evt.currentTarget.step;
+//   const amount = evt.currentTarget.amount;
+
+//   let timerId;
+//   let amountInter = Number(amount.value);
+//   let positionP = 1;
+//   let delayP = Number(delay.value);
+
+ 
+ 
+ 
+//   timerId = setInterval(() => {
+//     createPromise(positionP, delayP)
+//       .then(value => {
+//         console.log(
+//           `✅ Fulfilled promise ${value.position} in ${value.delay}ms`
+//         );
+//       })
+//       .catch(value => {
+//         console.log(
+//           `❌ Rejected promise ${value.position} in ${value.delay}ms`
+//         );
+//       });
+
+//     positionP += 1;
+//     amountInter -= 1;
+//     delayP += Number(step.value);
+
+//     if (!amountInter) {
+//       clearInterval(timerId);
+//     }
+//   }, Number(step.value));
+// }
 
 
